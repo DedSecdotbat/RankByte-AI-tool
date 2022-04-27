7d2546652305626314147effb0344b84faa2f5c7
Meet Radadiya <meetr501@gmail.com>
2022-04-27 14:39:20 +0530

    View Colleges feature

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import college_view
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('view_college_list/', college_view.view_college_list, name='view_college_list'),
 
 ]
